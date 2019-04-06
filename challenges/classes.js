2e03561327ec50c5d1b29c9716fb228811c30d83
Tim Dill <brodt7258@gmail.com>
2019-04-06 14:05:06 -0400

    classes stretch goal completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -45,3 +45,23 @@ console.log(cuboid.surfaceArea()); // 130
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
 // Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
 // Test your work by logging out your volume and surface area.
+
+// this assignment seems a bit silly.  Either I'm completely overwriting the method to use the 'size' property, and basically ignoring inheritance,
+// or I'm using inheritance, such as below, and have no need to change the methods at all.
+// and allowing access to length, width, and height directly would make no sense in something specified to be a cube.
+class CubeMaker extends CuboidMaker {
+  constructor({ size }) {
+    super({
+      length: size,
+      width: size,
+      height: size
+    });
+  }
+}
+
+const cube = new CubeMaker({
+  size: 4
+});
+
+console.log(cube.volume());
+console.log(cube.surfaceArea());
